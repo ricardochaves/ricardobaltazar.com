a8ee795490d109e3d3ea7ada21a7f75eaae183b2
ricardochaves <ricardobchaves6@gmail.com>
2022-12-16 19:59:45 -0300

    feat: add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,5 +42,25 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-NHMT2HT",
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 };
